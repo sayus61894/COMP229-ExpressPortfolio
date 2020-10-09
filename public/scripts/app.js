4db7d5a40b57a0fd69d70501c76da06da2743311
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2020-10-09 13:00:30 -0400

    Almost done with projects page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,46 @@
 // Custom JS
+$(function(){
 
-$('#missionStatement').on('click', function(){
-    console.log("Mission Statement Clicked");
-})
+
+    $('#missionStatement').on('click', function(){
+        console.log("Mission Statement Clicked");
+    })
+    
+    $('#project01').on('click', function(){
+        console.log("starting");
+        $("#project02").removeClass('active');
+        $("#project03").removeClass('active');
+        $('#project01').addClass('active');
+        console.log("worked");
+        $('#proj01').css({"display": "flex", "background-color":"rgb(225, 225, 225)", "color": "black"});
+        $('#proj02').css("display", "none");
+        $('#proj03').css("display", "none");
+    
+    })
+
+    $('#project02').on('click', function(){
+        console.log("starting");
+        $("#project01").removeClass('active');
+        $("#project03").removeClass('active');
+        $('#project02').addClass('active');
+        console.log("worked");
+        $('#proj02').css({"display": "flex", "background-color":"rgb(225, 225, 225)", "color": "black"});
+        $('#proj01').css("display", "none");
+        $('#proj03').css("display", "none");
+    
+    })
+
+    $('#project03').on('click', function(){
+        console.log("starting");
+        $("#project02").removeClass('active');
+        $("#project01").removeClass('active');
+        $('#project03').addClass('active');
+        console.log("worked");
+        $('#proj03').css({"display": "flex", "background-color":"rgb(225, 225, 225)", "color": "black"});
+        $('#proj02').css("display", 'none');
+        $('#proj01').css("display","none");
+    
+    })
+    
+
+});
