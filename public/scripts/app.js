3d7e31844ee61ad4ee7c8817c66389b9f8c858c3
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2020-10-09 19:46:45 -0400

    Added Comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,14 @@
+/*      
+        Filename:   app.js
+        Name:       Parth Shreyash Patel
+        StudentID:  301 134 072
+        Date:       2020-10-09
+*/
 
-// Custom JS
+
 $(function(){
     
     /* Trying to emulate the data-toggle function */
-
     /* Clicking the first tab, removes the active tab from the others, and adds it to itself.
         Then changes the display of the info windows to ensure that only the corresponding one is shown */ 
     $('#project01').on('click', function(){
@@ -15,7 +20,6 @@ $(function(){
         $('#proj01').css({"display": "flex", "background-color":"rgb(225, 225, 225)", "color": "black"});
         $('#proj02').css("display", "none");
         $('#proj03').css("display", "none");
-    
     });
 
     $('#project02').on('click', function(){
@@ -27,7 +31,6 @@ $(function(){
         $('#proj02').css({"display": "flex", "background-color":"rgb(225, 225, 225)", "color": "black"});
         $('#proj01').css("display", "none");
         $('#proj03').css("display", "none");
-    
     });
 
     $('#project03').on('click', function(){
@@ -39,28 +42,31 @@ $(function(){
         $('#proj03').css({"display": "flex", "background-color":"rgb(225, 225, 225)", "color": "black"});
         $('#proj02').css("display", 'none');
         $('#proj01').css("display","none");
-    
     });
 
     /* Helper functions for the 'Packages' for the Service Page. Originally display is set to 'none'.
         Clicking on a bar, will change the display of the corresponding 'package' thus showing it.
         Before showing the corresponding package, any packages being shown are first hidden */
     
+         /*for java bar and card */
     $('#java').on('click', function(){
             $(".serviceCard").hide();
             $('#javaCard').toggle();
     });
 
+        /*for C# bar and card */
     $('#cSharp').on('click', function(){
         $(".serviceCard").hide();
         $('#cSharpCard').toggle();
     });
 
+        /*for python bar and card */
     $('#python').on('click', function(){
         $(".serviceCard").hide();
         $('#pythonCard').toggle();
     });
 
+        /*for web dev bar and card. Group them into one class as they all correspond to a single package */
     $('.webDevGroup').on('click', function(){
         $(".serviceCard").hide();
         $('#webCard').toggle();
